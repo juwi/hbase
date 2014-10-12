149157be1d72bd3d8b7ea06451beb6b95dd20e9b
juwi <julianwissmann@gmail.com>
2014-10-12 02:58:45 +0200

    Implemented client-side max. It is largely built around an evil casting hack around the return type <R>. Don't see how to do it any other way without having to touch code deep in hbase, though. Therefor it will be the way forward for everything else.

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/coprocessor/TimeseriesAggregationClient.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/coprocessor/TimeseriesAggregationClient.java
@@ -1,6 +1,7 @@
 package org.apache.hadoop.hbase.client.coprocessor;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 import org.apache.commons.logging.Log;
@@ -18,18 +19,18 @@ import org.apache.hadoop.hbase.ipc.ServerRpcController;
 import org.apache.hadoop.hbase.protobuf.ProtobufUtil;
 import org.apache.hadoop.hbase.protobuf.generated.TimeseriesAggregateProtos.TimeseriesAggregateRequest;
 import org.apache.hadoop.hbase.protobuf.generated.TimeseriesAggregateProtos.TimeseriesAggregateResponse;
+import org.apache.hadoop.hbase.protobuf.generated.TimeseriesAggregateProtos.TimeseriesAggregateResponseMapEntry;
 import org.apache.hadoop.hbase.protobuf.generated.TimeseriesAggregateProtos.TimeseriesAggregateService;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import com.google.protobuf.ByteString;
 import com.google.protobuf.Message;
 
-
 /**
  * This client class is for invoking the aggregate functions deployed on the
- * Region Server side via the TimeseriesAggregateService. This class will implement the
- * supporting functionality for summing/processing the individual results
- * obtained from the TimeseriesAggregateService for each region.
+ * Region Server side via the TimeseriesAggregateService. This class will
+ * implement the supporting functionality for summing/processing the individual
+ * results obtained from the TimeseriesAggregateService for each region.
  * <p>
  * This will serve as the client side handler for invoking the aggregate
  * functions.
@@ -44,138 +45,181 @@ import com.google.protobuf.Message;
  * parameter type. For average and std, it returns a double value. For row
  * count, it returns a long value.
  * 
- * <T> Cell value data type
- * <S> Promoted data type
- * <P> PB message that is used to transport initializer specific bytes
- * <Q> PB message that is used to transport Cell (<T>) instance
- * <R> PB message that is used to transport Promoted (<S>) instance
+ * <T> Cell value data type <S> Promoted data type
+ * <P>
+ * PB message that is used to transport initializer specific bytes
+ * <Q>PB message that is used to transport Cell (<T>) instance <R> PB message
+ * that is used to transport Promoted (<S>) instance
  */
 @InterfaceAudience.Private
 public class TimeseriesAggregationClient {
 
-  private static final Log log = LogFactory.getLog(TimeseriesAggregationClient.class);
-  Configuration conf;
+	private static final Log log = LogFactory
+			.getLog(TimeseriesAggregationClient.class);
+	Configuration conf;
 
-  /**
-   * Constructor with Conf object
-   * @param cfg
-   */
-  public TimeseriesAggregationClient(Configuration cfg) {
-    this.conf = cfg;
-  }
-  
-  /**
-   * It gives the maximum value of a column for a given column family for the
-   * given range. In case qualifier is null, a max of all values for the given
-   * family is returned.
-   * @param tableName
-   * @param ci
-   * @param scan
-   * @return max val <R> (Will come as proto from region needs to be passed out as ConcurrentSkipListMap)
-   * @throws Throwable
-   *           The caller is supposed to handle the exception as they are thrown
-   *           & propagated to it.
-   */
-  public <R, S, P extends Message, Q extends Message, T extends Message> ConcurrentSkipListMap max(
-      final TableName tableName, final ColumnInterpreter<R, S, P, Q, T> ci, final Scan scan)
-      throws Throwable {
-    Table table = null;
-    try {
-      table = new HTable(conf, tableName);
-      return max(table, ci, scan);
-    } finally {
-      if (table != null) {
-        table.close();
-      }
-    }
-  }
+	/**
+	 * Constructor with Conf object
+	 * 
+	 * @param cfg
+	 */
+	public TimeseriesAggregationClient(Configuration cfg) {
+		this.conf = cfg;
+	}
 
-  /**
-   * It gives the maximum value of a column for a given column family for the
-   * given range. In case qualifier is null, a max of all values for the given
-   * family is returned.
-   * @param table
-   * @param ci
-   * @param scan
-   * @return max val <R> (Will come as proto from region needs to be passed out as ConcurrentSkipListMap)
-   * @throws Throwable
-   *           The caller is supposed to handle the exception as they are thrown
-   *           & propagated to it.
-   */
-  public <R, S, P extends Message, Q extends Message, T extends Message> 
-  ConcurrentSkipListMap max(final Table table, final ColumnInterpreter<R, S, P, Q, T> ci,
-      final Scan scan) throws Throwable {
-    final TimeseriesAggregateRequest requestArg = validateArgAndGetPB(scan, ci, false);
-    class MaxCallBack implements Batch.Callback<R> {
-      R max = null;
+	/**
+	 * It gives the maximum value of a column for a given column family for the
+	 * given range. In case qualifier is null, a max of all values for the given
+	 * family is returned.
+	 * 
+	 * @param tableName
+	 * @param ci
+	 * @param scan
+	 * @return max val <R> (Will come as proto from region needs to be passed
+	 *         out as ConcurrentSkipListMap)
+	 * @throws Throwable
+	 *             The caller is supposed to handle the exception as they are
+	 *             thrown & propagated to it.
+	 */
+	public <R, S, P extends Message, Q extends Message, T extends Message> ConcurrentSkipListMap max(
+			final TableName tableName,
+			final ColumnInterpreter<R, S, P, Q, T> ci, final Scan scan)
+			throws Throwable {
+		Table table = null;
+		try {
+			table = new HTable(conf, tableName);
+			return max(table, ci, scan);
+		} finally {
+			if (table != null) {
+				table.close();
+			}
+		}
+	}
 
-      R getMax() {
-        return max;
-      }
+	/**
+	 * It gives the maximum value of a column for a given column family for the
+	 * given range. In case qualifier is null, a max of all values for the given
+	 * family is returned.
+	 * 
+	 * @param table
+	 * @param ci
+	 * @param scan
+	 * @return max val <R> (Will come as proto from region needs to be passed
+	 *         out as ConcurrentSkipListMap)
+	 * @throws Throwable
+	 *             The caller is supposed to handle the exception as they are
+	 *             thrown & propagated to it.
+	 */
+	public <R, S, P extends Message, Q extends Message, T extends Message> ConcurrentSkipListMap<Long, R> max(
+			final Table table, final ColumnInterpreter<R, S, P, Q, T> ci,
+			final Scan scan) throws Throwable {
+		final TimeseriesAggregateRequest requestArg = validateArgAndGetPB(scan,
+				ci, false);
+		class MaxCallBack implements Batch.Callback<R> {
+			ConcurrentSkipListMap<Long, R> max = new ConcurrentSkipListMap<>();
 
-      @Override
-      public synchronized void update(byte[] region, byte[] row, R result) {
-        max = (max == null || (result != null && ci.compare(max, result) < 0)) ? result : max;
-      }
-    }
-    MaxCallBack aMaxCallBack = new MaxCallBack();
-    table.coprocessorService(TimeseriesAggregateService.class, scan.getStartRow(), scan.getStopRow(),
-        new Batch.Call<TimeseriesAggregateService, R>() {
-          @Override
-          public R call(TimeseriesAggregateService instance) throws IOException {
-            ServerRpcController controller = new ServerRpcController();
-            BlockingRpcCallback<TimeseriesAggregateResponse> rpcCallback = 
-                new BlockingRpcCallback<TimeseriesAggregateResponse>();
-            instance.getMax(controller, requestArg, rpcCallback);
-            TimeseriesAggregateResponse response = rpcCallback.get();
-            if (controller.failedOnException()) {
-              throw controller.getFailedOn();
-            }
-            if (response.getFirstPartCount() > 0) {
-              ByteString b = response.getFirstPart(0);
-              Q q = ProtobufUtil.getParsedGenericInstance(ci.getClass(), 3, b);
-              return ci.getCellValueFromProto(q);
-            }
-            return null;
-          }
-        }, aMaxCallBack);
-    return aMaxCallBack.getMax();
-  }
+			ConcurrentSkipListMap<Long, R> getMax() {
+				return max;
+			}
 
-  /*
-   * @param scan
-   * @param canFamilyBeAbsent whether column family can be absent in familyMap of scan
-   */
-  private void validateParameters(Scan scan, boolean canFamilyBeAbsent) throws IOException {
-    if (scan == null
-        || (Bytes.equals(scan.getStartRow(), scan.getStopRow()) && !Bytes
-            .equals(scan.getStartRow(), HConstants.EMPTY_START_ROW))
-        || ((Bytes.compareTo(scan.getStartRow(), scan.getStopRow()) > 0) &&
-          !Bytes.equals(scan.getStopRow(), HConstants.EMPTY_END_ROW))) {
-      throw new IOException(
-          "Agg client Exception: Startrow should be smaller than Stoprow");
-    } else if (!canFamilyBeAbsent) {
-      if (scan.getFamilyMap().size() != 1) {
-        throw new IOException("There must be only one family.");
-      }
-    }
-  }
-  
-  <R, S, P extends Message, Q extends Message, T extends Message> TimeseriesAggregateRequest 
-  validateArgAndGetPB(Scan scan, ColumnInterpreter<R,S,P,Q,T> ci, boolean canFamilyBeAbsent)
-      throws IOException {
-    validateParameters(scan, canFamilyBeAbsent);
-    final TimeseriesAggregateRequest.Builder requestBuilder = 
-        TimeseriesAggregateRequest.newBuilder();
-    requestBuilder.setInterpreterClassName(ci.getClass().getCanonicalName());
-    P columnInterpreterSpecificData = null;
-    if ((columnInterpreterSpecificData = ci.getRequestData()) 
-       != null) {
-      requestBuilder.setInterpreterSpecificBytes(columnInterpreterSpecificData.toByteString());
-    }
-    requestBuilder.setScan(ProtobufUtil.toScan(scan));
-    return requestBuilder.build();
-  }
+			@Override
+			public synchronized void update(byte[] region, byte[] row, R result) {
+				// max = (max == null || (result != null && ci.compare(max,
+				// result) < 0)) ? result : max;
+				List<TimeseriesAggregateResponseMapEntry> results = ((TimeseriesAggregateResponse) result)
+						.getEntryList();
+				for (TimeseriesAggregateResponseMapEntry entry : results) {
+					R candidate;
+					if (entry.getValue().getFirstPartCount() > 0) {
+						ByteString b = entry.getValue().getFirstPart(0);
+						Q q = null;
+						try {
+							q = ProtobufUtil.getParsedGenericInstance(
+									ci.getClass(), 3, b);
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						candidate = ci.getCellValueFromProto(q);
+						if (null != q) {
+							if (max.containsKey(entry.getKey())) {
+								R current = max.get(entry.getKey());
+								max.put(entry.getKey(),
+										(current == null || (candidate != null && ci
+												.compare(current, candidate) < 0)) ? candidate
+												: current);
+							} else {
+								max.put(entry.getKey(),
+										ci.getCellValueFromProto(q));
+							}
+						}
+					}
+				}
+			}
+		}
 
+		MaxCallBack aMaxCallBack = new MaxCallBack();
+		table.coprocessorService(TimeseriesAggregateService.class,
+				scan.getStartRow(), scan.getStopRow(),
+				new Batch.Call<TimeseriesAggregateService, R>() {
+					@Override
+					public R call(TimeseriesAggregateService instance)
+							throws IOException {
+						ServerRpcController controller = new ServerRpcController();
+						BlockingRpcCallback<TimeseriesAggregateResponse> rpcCallback = new BlockingRpcCallback<TimeseriesAggregateResponse>();
+						instance.getMax(controller, requestArg, rpcCallback);
+						TimeseriesAggregateResponse response = rpcCallback
+								.get();
+						if (controller.failedOnException()) {
+							throw controller.getFailedOn();
+						}
+						if (response.getEntryCount() > 0) {
+							return (R) response;
+						}
+						return null;
+					}
+				}, aMaxCallBack);
+		return aMaxCallBack.getMax();
+	}
+
+	/*
+	 * @param scan
+	 * 
+	 * @param canFamilyBeAbsent whether column family can be absent in familyMap
+	 * of scan
+	 */
+	private void validateParameters(Scan scan, boolean canFamilyBeAbsent)
+			throws IOException {
+		if (scan == null
+				|| (Bytes.equals(scan.getStartRow(), scan.getStopRow()) && !Bytes
+						.equals(scan.getStartRow(), HConstants.EMPTY_START_ROW))
+				|| ((Bytes.compareTo(scan.getStartRow(), scan.getStopRow()) > 0) && !Bytes
+						.equals(scan.getStopRow(), HConstants.EMPTY_END_ROW))) {
+			throw new IOException(
+					"Agg client Exception: Startrow should be smaller than Stoprow");
+		} else if (!canFamilyBeAbsent) {
+			if (scan.getFamilyMap().size() != 1) {
+				throw new IOException("There must be only one family.");
+			}
+		}
+	}
+
+	<R, S, P extends Message, Q extends Message, T extends Message> TimeseriesAggregateRequest validateArgAndGetPB(
+			Scan scan, ColumnInterpreter<R, S, P, Q, T> ci,
+			boolean canFamilyBeAbsent) throws IOException {
+		validateParameters(scan, canFamilyBeAbsent);
+		final TimeseriesAggregateRequest.Builder requestBuilder = TimeseriesAggregateRequest
+				.newBuilder();
+		requestBuilder
+				.setInterpreterClassName(ci.getClass().getCanonicalName());
+		P columnInterpreterSpecificData = null;
+		if ((columnInterpreterSpecificData = ci.getRequestData()) != null) {
+			requestBuilder
+					.setInterpreterSpecificBytes(columnInterpreterSpecificData
+							.toByteString());
+		}
+		requestBuilder.setScan(ProtobufUtil.toScan(scan));
+		return requestBuilder.build();
+	}
 
 }
