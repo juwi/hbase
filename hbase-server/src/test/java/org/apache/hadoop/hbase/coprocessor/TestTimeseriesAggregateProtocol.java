6e010cd62798139a641338975c05928d04d7e585
juwi <julianwissmann@gmail.com>
2014-10-20 23:15:47 +0200

    a first set of working tests for basic functionality.

--- hbase-server/src/test/java/org/apache/hadoop/hbase/coprocessor/TestTimeseriesAggregateProtocol.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/coprocessor/TestTimeseriesAggregateProtocol.java
@@ -252,8 +252,13 @@ public class TestTimeseriesAggregateProtocol {
     assertEquals(results, sums);
   }
   
+  
   /**
-   * give sum for the entire table.
+   * ***************Test cases for Avg *******************
+   */
+  
+  /**
+   * give avg for the entire table.
    * @throws Throwable
    */
   @Test(timeout = 300000)
@@ -282,6 +287,43 @@ public class TestTimeseriesAggregateProtocol {
     ConcurrentSkipListMap<Long, Double> avgs = aClient.avg(TEST_TABLE, ci, scan);
     assertEquals(results, avgs);
   }
+  
+  
+  /**
+   * ***************Test cases for Std *******************
+   */
+  
+  /**
+   * give Std for the entire table.
+   * @throws Throwable
+   */
+  @Test(timeout = 300000)
+  public void testStdWithValidRange() throws Throwable {
+    int TIME_LIMIT =
+        (int) ((new GregorianCalendar(2014, 10, 10, 2, 0, 0).getTime().getTime()) / 1000);
+    TimeseriesAggregationClient aClient =
+        new TimeseriesAggregationClient(conf, 900, TIME_BASELINE, TIME_LIMIT, KEY_FILTER_PATTERN);
+    Scan scan = new Scan();
+    scan.addFamily(TEST_FAMILY);
+    // scan.setStartRow(START_ROW);
+    // scan.setStopRow(STOP_ROW);
+    final ColumnInterpreter<Long, Long, EmptyMsg, LongMsg, LongMsg> ci =
+        new LongColumnInterpreter();
+    Map<Long, Double> results = new ConcurrentSkipListMap<>();
+    results.put(1415574000000l,12.00d);
+    results.put(1415574900000l, 37.00d);
+    results.put(1415575800000l, 62.00d);
+    results.put(1415576700000l, 87.00d);
+    results.put(1415577600000l, 12.00d);
+    results.put(1415578500000l, 37.00d);
+    results.put(1415579400000l, 62.00d);
+    results.put(1415580300000l, 87.00d);
+    results.put(1415581200000l, 0.0d);
+    
+    ConcurrentSkipListMap<Long, Double> stds = aClient.std(TEST_TABLE, ci, scan);
+    assertEquals(results, stds);
+  }
+
 
 
 
